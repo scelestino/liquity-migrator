3e07106c87b716fc289f8833ff787d8a766cbada
Bruno Bonanno <bbonanno@gmail.com>
2021-06-07 01:20:05 +0200

    add alchemy secrets for CI

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,11 +10,7 @@ dotEnvConfig();
 // TODO: reenable solidity-coverage when it works
 // import "solidity-coverage";
 
-const INFURA_API_KEY = process.env.INFURA_API_KEY || "";
 const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY || "";
-const RINKEBY_PRIVATE_KEY =
-    process.env.RINKEBY_PRIVATE_KEY! ||
-    "0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3"; // well known private key
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 const config: HardhatUserConfig = {
@@ -30,10 +26,6 @@ const config: HardhatUserConfig = {
             }
         },
         localhost: {},
-        rinkeby: {
-            url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
-            accounts: [RINKEBY_PRIVATE_KEY],
-        },
         coverage: {
             url: "http://127.0.0.1:8555", // Coverage launches its own ganache-cli client
         },
